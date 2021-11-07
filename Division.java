d0ed8a9c1ba2d936147de6be89a7a45e5722b7a3
Ish2012 <2020MT93669@wilp.bits-pilani.ac.in>
2021-11-07 15:08:59 +0530

    Check condition for divison by 0

--- Division.java
+++ Division.java
@@ -3,7 +3,14 @@ public class Division
 
     public void ratio(int num1,int num2)
     {
-        System.out.println("Ratio: " + (num1 / num2));        
+        if(num2!=0)
+        {
+        System.out.println("Ratio: " + (num1 / num2));     
+        }
+        else
+        {
+            System.out.println("number can not be divided by 0");
+        }  
     }
     
 }
