09b23b8ebbe2e6cd67b41b89b5f3ace32663f301
93700bits <2020mt93700@wilp.bits-pilani.ac.in>
2021-11-07 15:13:10 +0530

    Updated code for displaying user friendly output

--- Multiplication.java
+++ Multiplication.java
@@ -3,7 +3,7 @@ public class Multiplication
 
     public void product(int num1,int num2) 
     {        
-        System.out.println("Product: " + (num1 * num2));        
+        System.out.println("The product of "+num1+ " and "+num2+" is : "  + (num1 * num2));      
     }
     
 }
